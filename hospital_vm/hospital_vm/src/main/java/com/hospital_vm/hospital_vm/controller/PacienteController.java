7503ac87796d1ef494228933c2f687b6dabfe2fb
jorgeguzmansandoval-32 <jorgeguzmansandoval1@gmail.com>
2026-04-23 22:13:24 -0400

    version 4

--- hospital_vm/hospital_vm/src/main/java/com/hospital_vm/hospital_vm/controller/PacienteController.java
+++ hospital_vm/hospital_vm/src/main/java/com/hospital_vm/hospital_vm/controller/PacienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,5 +72,18 @@ public class PacienteController {
     }
   }
 
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> eliminarPaciente(@PathVariable Integer id){
+    try{
+        pacienteService.findById(id);
+        pacienteService.delete(id);
+        return ResponseEntity.noContent().build();
+        
+    }catch (Exception e) {
+        return ResponseEntity.notFound().build();
+    }
+
+  }
 
 }
